77dcc15bbce618ccf56f7afa04ac36ccd2424613
Dmitry <d2013.dima@mail.ru>
2025-02-09 21:42:50 +0500

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Button size={"lg"} variant={"destructive"}>
         Hellow
       </Button>
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
     </div>
   );
 }
